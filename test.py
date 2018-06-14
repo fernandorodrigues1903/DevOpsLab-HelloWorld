03467103880021ec52605004d35b381e26bfcee7
fernandoantunesfju <fernandoantunesfju@gmail.com>
2018-06-13 22:05:38 -0400

    Nova alteracao dos arquivos index e test py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Gremio Campeao do Mundo")
+        self.assertRegex(result.data.decode(), "Fernando Teste XXX")
 
 
 if __name__ == "__main__":
